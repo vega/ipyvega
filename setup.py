3500f19e47d36586827ff374f0c95f331c1b9f74
Dominik Moritz <domoritz@gmail.com>
2015-09-02 17:06:19 -0700

    Rename to vega. Fixes #12

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='vegawidget',
+    name='vega',
     version='0.1',
-    description='An IPython widget for polestar and voyager',
+    description='An IPython widget for vega-lite, polestar, and voyager',
     author='Dominik Moritz, Kanit "Ham" Wongsuphasawat',
     author_email='domoritz@gmail.com',
     license='MIT License',
@@ -16,8 +16,8 @@ setup(
     keywords='data visualization',
     classifiers=['Development Status :: 4 - Beta',
                  'Programming Language :: Python :: 2.7',
-                 'License :: OSI Approved :: MIT License'],
-    packages=['vegawidget'],
+                 'License :: OSI Approved :: BSD License'],
+    packages=['vega'],
     package_data={'': ['static/*.js',
                        'static/*.css']}
 )
