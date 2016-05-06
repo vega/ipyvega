0f5ffea0b93617d5db833120f8a83d6483769744
Dominik Moritz <domoritz@gmail.com>
2016-05-05 21:40:37 -0400

    Python 3 compatible.

--- vega/utils.py
+++ vega/utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 
 def update(d, u):
-    for k, v in u.iteritems():
+    for k, v in u.items():
         if isinstance(v, collections.Mapping):
             r = update(d.get(k, {}), v)
             d[k] = r
--- vega/vegalite.py
+++ vega/vegalite.py
@@ -1,8 +1,10 @@
+from __future__ import absolute_import
+
 import json
 
 from IPython.display import display
 
-import utils
+from . import utils
 
 TEMPLATE = "static/vega-lite.html"
 
