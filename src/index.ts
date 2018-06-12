6c324bc976234da448970ea91ba0ec8f9f840f3b
Dominik Moritz <domoritz@gmail.com>
2018-06-12 10:53:19 -0700

    Send same-origin cookies with Vega loader requests. See jupyterlab/jupyterlab#4707

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import vegaEmbed, { Mode, EmbedOptions } from 'vega-embed';
-import { Spec } from 'vega-lib';
-import { TopLevelSpec } from 'vega-lite';
+import vegaEmbed, { Mode, EmbedOptions } from "vega-embed";
+import { Spec } from "vega-lib";
+import { TopLevelSpec } from "vega-lite";
 
 function javascriptIndex(selector: string, outputs) {
   // Return the index in the output array of the JS repr of this viz
   for (let i = 0; i < outputs.length; i++) {
     const item = outputs[i];
-    if (item.metadata &&
-      item.metadata['jupyter-vega'] === selector &&
-      item.data['application/javascript'] !== undefined) {
+    if (
+      item.metadata &&
+      item.metadata["jupyter-vega"] === selector &&
+      item.data["application/javascript"] !== undefined
+    ) {
       return i;
     }
   }
@@ -19,9 +21,11 @@ function imageIndex(selector: string, outputs) {
   // Return the index in the output array of the PNG repr of this viz
   for (let i = 0; i < outputs.length; i++) {
     const item = outputs[i];
-    if (item.metadata &&
-      item.metadata['jupyter-vega'] === selector &&
-      item.data['image/png'] !== undefined) {
+    if (
+      item.metadata &&
+      item.metadata["jupyter-vega"] === selector &&
+      item.data["image/png"] !== undefined
+    ) {
       return i;
     }
   }
@@ -38,7 +42,13 @@ function showError(el: HTMLElement, error: Error) {
   throw error;
 }
 
-export function render(selector: string, spec: Spec | TopLevelSpec, type: Mode, opt: EmbedOptions, output_area) {
+export function render(
+  selector: string,
+  spec: Spec | TopLevelSpec,
+  type: Mode,
+  opt: EmbedOptions,
+  output_area
+) {
   // Find the indices of this visualizations JS and PNG
   // representation.
   const imgIndex = imageIndex(selector, output_area.outputs);
@@ -46,27 +56,37 @@ export function render(selector: string, spec: Spec | TopLevelSpec, type: Mode, 
 
   // If we have already rendered a static image, don't render
   // the JS version or append a new PNG version
-  if (imgIndex > -1 && jsIndex > -1 && imgIndex === (jsIndex + 1)) {
+  if (imgIndex > -1 && jsIndex > -1 && imgIndex === jsIndex + 1) {
     return;
   }
 
   // Never been rendered, so render JS and append the PNG to the
   // outputs for the cell
   const el = document.getElementById(selector.substring(1));
-  vegaEmbed(el, spec, { defaultStyle: true, ...opt, mode: type }).then((result) => {
-    const imageData = result.view.toImageURL('png').then((imageData) => {
-      if (output_area !== undefined) {
-        const output = {
-          data: {
-            'image/png': imageData.split(',')[1]
-          },
-          metadata: { 'jupyter-vega': selector },
-          output_type: 'display_data'
-        };
-        // This appends the PNG output, but doesn't render it this time
-        // as the JS version will be rendered already.
-        output_area.outputs.push(output);
-      }
-    }).catch(error => showError(el, error));
-  }).catch(error => showError(el, error));
+  vegaEmbed(el, spec, {
+    defaultStyle: true,
+    loader: { http: { credentials: "same-origin" } },
+    ...opt,
+    mode: type
+  })
+    .then(result => {
+      const imageData = result.view
+        .toImageURL("png")
+        .then(imageData => {
+          if (output_area !== undefined) {
+            const output = {
+              data: {
+                "image/png": imageData.split(",")[1]
+              },
+              metadata: { "jupyter-vega": selector },
+              output_type: "display_data"
+            };
+            // This appends the PNG output, but doesn't render it this time
+            // as the JS version will be rendered already.
+            output_area.outputs.push(output);
+          }
+        })
+        .catch(error => showError(el, error));
+    })
+    .catch(error => showError(el, error));
 }
