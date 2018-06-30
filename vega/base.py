d2fe6e9e8ea641a165d7921bb5d99ce50f8f52f9
CMP <c.prohm@googlemail.com>
2018-06-30 18:47:40 +0200

    Display via single mime-bundle without side effects

--- vega/base.py
+++ vega/base.py
@@ -13,7 +13,6 @@ class VegaBase(object):
     """A custom Vega-Lite display object."""
 
     JS_TEMPLATE = "static/vega.js"
-    HTML_TEMPLATE = "static/vega.html"
     render_type = ''  # vega or vega-lite
 
     def __init__(self, spec, data=None, opt=None):
@@ -25,31 +24,24 @@ class VegaBase(object):
     def _prepare_spec(self, spec, data):
         return spec
 
-    def _generate_html(self, id):
-        template = utils.get_content(self.HTML_TEMPLATE)
-        return template.format(id=id)
-
     def _generate_js(self, id, **kwds):
         template = utils.get_content(self.JS_TEMPLATE)
         selector = '#{0}'.format(id)
         payload = template.format(
             selector=selector,
+            id=json.dumps(str(id)),
             spec=json.dumps(self.spec, **kwds),
             opt=json.dumps(self.opt, **kwds),
             type=self.render_type
         )
         return payload
 
-    def _ipython_display_(self):
+    def _repr_mimebundle_(self, include=None, exclude=None):
         """Display the visualization in the Jupyter notebook."""
         id = uuid.uuid4()
-        publish_display_data(
-            {'text/html': self._generate_html(id)},
-            metadata={'jupyter-vega': '#{0}'.format(id)}
-        )
-        publish_display_data(
+        return (
             {'application/javascript': self._generate_js(id)},
-            metadata={'jupyter-vega': '#{0}'.format(id)}
+            {'jupyter-vega': '#{0}'.format(id)},
         )
 
     def display(self):
--- vega/static/vega.js
+++ vega/static/vega.js
@@ -6,6 +6,21 @@ var type = "{type}";
 var output_area = this;
 
 require(['nbextensions/jupyter-vega/index'], function(vega) {{
+  var target = document.createElement('div');
+  target.id = {id};
+  target.className = 'vega-embed';
+
+  var style = document.createElement('style');
+  style.textContent = [
+    '.vega-embed .error p {{',
+    '  color: firebrick;',
+    '  font-size: 14px;',
+    '}}',
+  ].join('\\n');
+
+  element[0].appendChild(target);
+  element[0].appendChild(style);
+
   vega.render(selector, spec, type, opt, output_area);
 }}, function (err) {{
   if (err.requireType !== 'scripterror') {{
