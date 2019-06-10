ede28caade6f97dfb663d014c12c4717f408492b
Christopher Prohm <chmp@users.noreply.github.com>
2019-06-10 22:21:39 +0200

    Implementation of ipython widget (#104)
    * Add implementation of ipython widget
    
    * Address review comments
    
    * Fix optional dependencies with webpack
    
    * Fix typescript style
    
    * Handle updates before widget is rendered correctly
    
    * Add lightweight error messaging for updates
    
    * Add description to notebook
    
    * Call finalize when replacing the view
    
    Co-Authored-By: Dominik Moritz <domoritz@gmail.com>
    
    * Fix error in finalize call, rebuild static js
    
    * Remove from_chart and update docstring accordingly
    
    * Make TODO comment re validation more self-explanatory
    
    * Allow to set embedding options
    
    * Add point how to select between vega / vega-lite
    
    * remove defaultStyle

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import vegaEmbed, { Mode, EmbedOptions } from "vega-embed";
-import { Spec } from "vega";
+import { Spec, View } from "vega";
 import { TopLevelSpec } from "vega-lite";
 
 export { default as vegaEmbed } from "vega-embed";
@@ -91,3 +91,107 @@ export function render(
     })
     .catch(error => showError(el, error));
 }
+
+interface WidgetUpdate {
+  key: string;
+  remove?: string;
+  insert?: any[];
+}
+
+interface WidgetUpdateMessage {
+  type: "update";
+  updates: WidgetUpdate[];
+}
+
+// validate the ev object and cast it to the correct type
+function checkWidgetUpdate(ev: any): WidgetUpdateMessage | null {
+  if (ev.type != "update") {
+    return null;
+  }
+
+  // TODO: Fully validate ev and give a easy to understand error message if it is ill-formed
+  return ev as WidgetUpdateMessage;
+}
+
+// NOTE: juggle to support optional dependencies that don't break webpack and jupyter
+var VegaWidgetDef: any = null;
+if (__webpack_modules__[require.resolveWeak("@jupyter-widgets/base")]) {
+  const widgets = require("@jupyter-widgets/base");
+
+  VegaWidgetDef = widgets.DOMWidgetView.extend({
+    render: function() {
+      this.viewElement = document.createElement("div");
+      this.el.appendChild(this.viewElement);
+
+      this.errorElement = document.createElement("div");
+      this.errorElement.style.color = "red";
+      this.el.appendChild(this.errorElement);
+
+      const reembed = () => {
+        if (this.view) {
+          this.view.finalize();
+          this.view = null;
+        }
+        const spec = JSON.parse(this.model.get("_spec_source"));
+        const opt = JSON.parse(this.model.get("_opt_source"));
+
+        if (spec == null) {
+          return;
+        }
+
+        vegaEmbed(this.viewElement, spec, {
+          loader: { http: { credentials: "same-origin" } },
+          ...opt
+        })
+          .then(({ view }) => {
+            this.view = view;
+            this.send({ type: "display" });
+          })
+          .catch(err => console.error(err));
+      };
+
+      const applyUpdate = async (update: WidgetUpdate) => {
+        if (this.view == null) {
+          throw new Error("Internal error: no view attached to widget");
+        }
+
+        const filter = new Function(
+          "datum",
+          "return (" + (update.remove || "false") + ")"
+        );
+        const newValues = update.insert || [];
+        const changeSet = this.view
+          .changeset()
+          .insert(newValues)
+          .remove(filter);
+
+        await this.view.change(update.key, changeSet).runAsync();
+      };
+
+      const applyUpdates = async (message: WidgetUpdateMessage) => {
+        for (const update of message.updates) {
+          await applyUpdate(update);
+        }
+      };
+
+      this.model.on("change:_spec_source", reembed);
+      this.model.on("change:_opt_source", reembed);
+      this.model.on("msg:custom", (ev: any) => {
+        const message = checkWidgetUpdate(ev);
+        if (message == null) {
+          return;
+        }
+
+        applyUpdates(message).catch((err: Error) => {
+          this.errorElement.textContent = "" + err;
+          console.error(err);
+        });
+      });
+
+      // initial rendering
+      reembed();
+    }
+  });
+}
+
+export const VegaWidget = VegaWidgetDef;
