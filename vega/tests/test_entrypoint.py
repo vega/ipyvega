c945228b33c4c574b496570024348dd1e1c69011
Jake Vanderplas <jakevdp@google.com>
2020-03-31 23:08:09 -0700

    TST: add test of altair entrypoints (#187)

---
+++ vega/tests/test_entrypoint.py
@@ -0,0 +1,17 @@
+import altair as alt
+import altair.vega as alt_vg
+from vega import vega, vegalite
+
+import pytest
+
+
+def test_entrypoint_exists():
+    assert "notebook" in alt.renderers.names()
+    assert "notebook" in alt_vg.renderers.names()
+
+
+def test_entrypoint_identity():
+    with alt.renderers.enable("notebook"):
+        assert alt.renderers.get() is vegalite.entry_point_renderer
+    with alt.vega.renderers.enable("notebook"):
+        assert alt_vg.renderers.get() is vega.entry_point_renderer
