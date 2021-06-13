44efd02e507f91516a8f1a36020ad7aabba4b51d
Christian Poli <Christian.Poli@inria.fr>
2021-06-13 15:07:06 +0200

    using NDArray instead of DataUnion

--- vega/widget.py
+++ vega/widget.py
@@ -4,8 +4,7 @@ import contextlib
 import json
 import uuid
 import sys
-from ipydatawidgets import DataUnion
-from ipydatawidgets.widgets import DataWidget
+from ipydatawidgets import NDArray
 from ipydatawidgets.ndarray.serializers import (
     array_to_compressed_json,
     array_from_compressed_json)
@@ -28,31 +27,9 @@ except ImportError as err:
 
 __all__ = ['VegaWidget']
 
-#
-# The two functions below  (data_union_to_json_compress, data_union_from_json_compress)
-# are adapted from ipydatawidgets
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-#
-
-
-def data_union_to_json_compress(value, widget):
-    """Serializer for union of NDArray and NDArrayWidget"""
-    if isinstance(value, widgets.Widget):
-        return widgets.widget_serialization['to_json'](value, widget)
-    return array_to_compressed_json(value, widget)
-
-
-def data_union_from_json_compress(value, widget):
-    """Deserializer for union of NDArray and NDArrayWidget"""
-    if isinstance(value, str) and value.startswith('IPY_MODEL_'):
-        return widgets.widget_serialization['from_json'](value, widget)
-    return array_from_compressed_json(value, widget)
-
-
 _serialization = dict(
-    to_json=data_union_to_json_compress,
-    from_json=data_union_from_json_compress)
+    to_json=array_to_compressed_json,
+    from_json=array_from_compressed_json)
 
 class VegaWidget(DOMWidget):
     """An IPython widget display a vega chart.
@@ -101,7 +78,7 @@ class VegaWidget(DOMWidget):
     _model_module_version = Unicode('0.1.0').tag(sync=True)
     _spec_source = Unicode('null').tag(sync=True)
     _opt_source = Unicode('null').tag(sync=True)
-    _df = DataUnion(
+    _df = NDArray(
         np.array([], dtype='float32'),
         dtype='float32'
     ).tag(sync=True, **_serialization)
