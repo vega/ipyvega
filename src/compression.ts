88e5138faadcd803fb6f72a7f60960d929260353
Christian Poli <Christian.Poli@inria.fr>
2021-06-18 13:05:06 +0200

    adding lz4js decompression

--- src/compression.ts
+++ src/compression.ts
@@ -1,15 +1,12 @@
 import pako = require('pako');
-//import buffer = require('buffer');
-//var Buffer = buffer.Buffer;
-//import lz4 = require('lz4');
-
+import lz4 = require("lz4js");
 export
 const decompress : {[key:string]: Function} = {
       zlib: (input: ArrayBuffer): ArrayBuffer => {
            return pako.inflate(new Uint8Array(input)).buffer;
          },
-      /*lz4: (input: ArrayBuffer): ArrayBuffer => {
-          return lz4.decode(input);
-         },*/
+      lz4: (input: ArrayBuffer): ArrayBuffer => {
+          return lz4.dcompress(new Uint8Array(input)).buffer;
+         },
 
 }
