8e13aa75e9892f0d90c048b117824acf714efbff
Carson <cpsievert1@gmail.com>
2022-01-19 16:31:28 -0600

    Properly copy distributed JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const version = require('./package.json').version;
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = [
   {
     entry: "./src/index.ts",
     output: {
       filename: "index.js",
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "vega", "static"),
       libraryTarget: "amd",
       publicPath: 'https://unpkg.com/jupyter-vega@' + version + '/dist/'
     },
     resolve: { extensions: [".ts", ".tsx", ".js"] },
     module: { rules: [{ test: /\.tsx?$/, loader: "ts-loader" }] },
     externals: ["@jupyter-widgets/base"],
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+      new FileManagerPlugin({
+        events: {
+          onEnd: { 
+            copy: [
+              { source: './src/vega.js', destination: './vega/static/vega.js' },
+              { source: './vega/static/*', destination: './dist' }
+            ]
+          }
+        }
+      })
+    ]
   }
 ];
