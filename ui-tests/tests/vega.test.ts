d6bc6d0f8308f832ea23d0335783009561589fba
Christian Poli <Christian.Poli@inria.fr>
2023-02-16 17:04:39 +0100

    update the screenshots, temporarily silence the warnings in NBTest.ipynb, cleanup trace in vega.test.ts

--- ui-tests/tests/vega.test.ts
+++ ui-tests/tests/vega.test.ts
@@ -53,7 +53,6 @@ describe('Vega UI test', () => {
     await galata.notebook.runCellByCell({
       onAfterCellRun: async (cellIndex: number) => {
         const cell = await galata.notebook.getCellOutput(cellIndex);
-	      console.log("CELL:", cell);
         if (cell) {
           if (
             await galata.capture.screenshot(
